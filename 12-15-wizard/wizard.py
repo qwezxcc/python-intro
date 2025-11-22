bef7ffd615b30e637f075b96be8044ff8f9a07a2
Alex9003 <sasha.morhush.z@gmail.com>
2025-11-22 16:18:12 +0200

    1

--- 12-15-wizard/wizard.py
+++ 12-15-wizard/wizard.py
@@ -1,6 +1,7 @@
+from tkinter.constants import RIGHT
+
 import pygame
 import random
-
 
 # --------------------------------
 IMAGES_PATH: str = 'images/'
@@ -22,6 +23,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Wizard')
 clock = pygame.time.Clock()
 
+
 # === Клас персонажу ===
 class Wizard:
     WIDTH = 150
@@ -35,32 +37,84 @@ class Wizard:
         self.y = self.START_Y
 
         self.stop_image = pygame.image.load(IMAGES_PATH + 'wizard_red/stop_right.png')
+
+        self.left_frames = [
+            pygame.image.load(IMAGES_PATH + f"wizard_red/left_{i}.png")
+            for i in range(1, 5)
+        ]
+
+        self.right_frames = [
+            pygame.image.load(IMAGES_PATH + f"wizard_red/right_{i}.png")
+            for i in range(1, 5)
+        ]
+
         self.image = self.stop_image
+        self.is_moving = False
+        self.frame_index = 0
+        self.last_frame_time = 0
+        self.current_frames = self.right_frames
 
     def show(self):
         screen.blit(self.image, (self.x, self.y))
-    def move_left(self):
-        pass
-    def move_right(self):
-        pass
-    def move(self):
-        pass
-    def update_animation(self):
-        pass
 
+    def move_left(self):
+        if self.x - self.SPEED >= 0:
+            self.x -= self.SPEED
+        else:
+            self.x = 0
+
+        print('L', self.x)
+
+    def move_right(self):
+        if self.x + self.SPEED <= SCREEN_WIDTH - self.WIDTH:
+            self.x += self.SPEED
+        else:
+            self.x = SCREEN_WIDTH - self.WIDTH
+
+        print('R', self.x)
+
+    def move(self, keys_pressed: pygame.key.ScancodeWrapper, current_time: int):
+
+        moving_left = keys_pressed[pygame.K_LEFT]
+        moving_right = keys_pressed[pygame.K_RIGHT]
+
+        # print(moving_left, moving_right)
+
+        self.is_moving = moving_left or moving_right
+
+        if moving_left and not moving_right:
+            self.current_frames = self.left_frames
+            self.move_left()
+        elif moving_right and not moving_left:
+            self.current_frames = self.right_frames
+            self.move_right()
+
+        self.update_animation(current_time)
+
+    def update_animation(self, current_time):
+        if self.is_moving:
+            if current_time - self.last_frame_time >= 100:
+                self.frame_index = ((self.frame_index + 1) % len(self.current_frames))
+                self.image = self.current_frames[self.frame_index]
+                self.last_frame_time = current_time
+        else:
+            self.image = self.stop_image
 
 
 # === Клас діаманта ===
 class Diamond:
     pass
 
+
 # === Менеджер діамантів ===
 class Diamonds:
     pass
 
+
 # === Візуальні ефекти ===
 class FloatingText:
     pass
+
 
 # === Основна гра ===
 class Game:
@@ -78,28 +132,29 @@ class Game:
         pygame.time.set_timer(DIAMOND_EVENT, random.randint(DIAMOND_SPAWN_TIME_MIN_MS, DIAMOND_SPAWN_TIME_MAX_MS))
 
     def _draw_background(self):
-        screen.blit(self.background, (0,0))
+        screen.blit(self.background, (0, 0))
 
     def play(self):
         while self.run:
+            current_time = pygame.time.get_ticks()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.run = False
 
-
-            #
-            keys = pygame.key.get_pressed()
+            keys_pressed = pygame.key.get_pressed()
+            self.player.move(keys_pressed, current_time)
 
             # drawing events on screen
             self._draw_background()
             self.player.show()
 
-
             pygame.display.update()
             clock.tick(FPS)
-        #end while
+        # end while
 
         pygame.quit()
+
 
 if __name__ == "__main__":
     game = Game()
